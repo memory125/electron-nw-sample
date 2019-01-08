0e19e3ddb81a1d9075b37525e4541229f225d552
De Meng <mengde1120@sina.com>
2019-01-08 17:58:14 +0800

    update hello electron sample

--- hello-electron/main.js
+++ hello-electron/main.js
@@ -1,20 +1,17 @@
 'use strict';
 
 const electron = require('electron');
-const app = electron.app;
+const app = electron.app;                                               
 const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow = null;
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin')
-        app.quit();
+  if (process.platform !== 'darwin') app.quit();
 });
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow();
-    mainWindow.loadURL('file://${__dirname}/index.html');
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
+  mainWindow = new BrowserWindow();
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.on('closed', () => { mainWindow = null; });
 });
